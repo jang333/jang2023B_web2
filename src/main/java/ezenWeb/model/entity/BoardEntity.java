2bfd952b5bc7d57d4343ad79905f188fef25ab2f
jang333 <tidls27@naver.com>
2024-03-21 17:09:47 +0900

    2024-03-21 엔티티

--- src/main/java/ezenWeb/model/entity/BoardEntity.java
+++ src/main/java/ezenWeb/model/entity/BoardEntity.java
@@ -1,21 +1,44 @@
 package ezenWeb.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @Entity
+@Table(name = "board")
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class BoardEntity {
-    @Id
-    private int bno;
+    @Id //pk키 명시
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto_increment
+    private int bno;        //게시물번호 pk
+    @Column(name = "title", length = 10, nullable = false, unique = true)
     private String btitle;
+
+    private boolean 필드0;
+
+    private byte 필드1;
+    private short 필드2;
+    private long 필드3;
+
+    private char 필드4;
+
+    private float 필드5;
+    private double 필드6;
+
+    private Date 필드7;
+    private LocalDateTime 필드8;
+
+
+
 
 }
 
